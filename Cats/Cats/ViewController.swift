9cb999ebd8e42d78ff13a2d65c35e0b2bfba38f9
EnriqueTor <etorrendell@gmail.com>
2016-10-05 21:54:17 -0400

    Complete with new name of cats from A to E

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    var cats: [String] = ["Abyssinian","American Bobtail","American Curl","American Shorthair","American Wirehair","Balinese","Bengal Cats","Birman","Bombay","British Shorthair","Burmese","Burmilla","Chartreux","Chinese Li Hua","Colorpoint Shorthair","Cornish Rex", "Cymric", "Devon Rex", "Egyptian Mau","European Burmese","Exotic"]
     
-   
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cats.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "kittyCell", for: indexPath)
+        let catName = cats[indexPath.row]
+        cell.textLabel?.text = catName
+        return cell
+        
+        
+    }
 }
